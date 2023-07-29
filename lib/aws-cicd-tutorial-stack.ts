f475741a9de83861d78d6d1facaf0cb87582d4b0
Jack (Pixegami) <pixegami@gmail.com>
2023-07-29 22:46:30 +1000

    add lambda function

--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class AwsCicdTutorialStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCicdTutorialQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionUrl.url,
+    });
   }
 }
