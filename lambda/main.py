0ec1a8e4ddb6a1b3599c7af065571d0fc479aaee
Jack (Pixegami) <pixegami@gmail.com>
2023-07-29 23:23:19 +1000

    add emoji

--- lambda/main.py
+++ lambda/main.py
@@ -3,5 +3,5 @@ import os
 
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
-    response_body = {"message": "Hello World", "version": version}
+    response_body = {"message": "Hello World 👋", "version": version}
     return {"statusCode": 200, "body": response_body}
