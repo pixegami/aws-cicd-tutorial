5dfcc225589fd2bb57006021661dc36a4efe9df9
Jack (Pixegami) <pixegami@gmail.com>
2023-07-29 23:16:39 +1000

    add version

--- lambda/main.py
+++ lambda/main.py
@@ -1,3 +1,7 @@
+import os
+
+
 def handler(event, context):
-    response_body = {"message": "Hello World", "version": "1.0.0"}
+    version = os.environ.get("VERSION", "0.0")
+    response_body = {"message": "Hello World", "version": version}
     return {"statusCode": 200, "body": response_body}
--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -1,15 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dotenv from "dotenv";
 
 export class AwsCicdTutorialStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Load the environment .env file.
+    dotenv.config();
+
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("lambda"),
       handler: "main.handler",
+      environment: {
+        VERSION: process.env.VERSION || "0.0",
+      },
     });
 
     const functionUrl = lambdaFunction.addFunctionUrl({
